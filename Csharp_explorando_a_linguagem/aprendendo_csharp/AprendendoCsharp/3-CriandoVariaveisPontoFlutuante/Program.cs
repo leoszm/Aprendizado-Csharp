50c5dc46e76f5c7636fd1c81c35e331b18e6f764
leoszm <leonardosousa60@gmail.com>
2023-01-15 17:43:00 -0300

    commit aula 3 concluida - variaveis de ponto flutuante

---
+++ Csharp_explorando_a_linguagem/aprendendo_csharp/AprendendoCsharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        //Declarações
+        double salario;
+        salario = 3000.10;
+        double idade1, idade2;
+        //Com(1) e sem ponto flutuante(2)
+        idade1 = 7.0 / 5;
+        idade2 = 7 / 5;
+        //Saída
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+        Console.WriteLine("Minha idade com ponto flutuante é de "  + idade1 + ".");
+        Console.WriteLine("Minha idade sem ponto flutuante é de " + idade2 + ".");
+        Console.WriteLine("Meu salário é de R$"+ salario + " reais.");
+        //Fim
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
