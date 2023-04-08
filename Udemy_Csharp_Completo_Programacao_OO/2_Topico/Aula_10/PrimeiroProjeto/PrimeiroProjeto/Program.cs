8e2dcb7e43e93472104e4010769dd6eb60e32dc4
leoszm <leonardosousa60@gmail.com>
2023-04-07 22:28:44 -0300

    first project PrimeiroProjeto added on folder Udemy

---
+++ Udemy_Csharp_Completo_Programacao_OO/2_Topico/Aula_10/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; //already included on .Net6
+
+namespace PrimeiroProjeto  //Name of project
+{
+    internal class Program //actually file name
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); //print on prompt this string content
+        }
+    }
+}
