1e04d7c8711cfc8553e89810439f613d2fda7579
leoszm <leonardosousa60@gmail.com>
2023-04-07 22:45:15 -0300

    extra comments on file Program of PrimeiroProjeto

--- Udemy_Csharp_Completo_Programacao_OO/2_Topico/Aula_10/PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ Udemy_Csharp_Completo_Programacao_OO/2_Topico/Aula_10/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,12 +1,20 @@
-﻿using System; //already included on .Net6
+﻿//System contém Funções basicas de manipulação de programas
+using System; //already included on .Net6
 
+//namespace é o agrupamento de classes relacionadas
 namespace PrimeiroProjeto  //Name of project
 {
-    internal class Program //actually file name
+    class Program //actually file name
     {
-        static void Main(string[] args)
+        //membro da classe
+        static void Main(string[] args) 
         {
             Console.WriteLine("Hello World!"); //print on prompt this string content
         }
     }
+    #region Comentários
+    //System contém Funções basicas de manipulação de programas
+    //Namespace é o agrupamento de classes relacionadas
+    //Static void Main é o entry point da aplicação, tudo que está dentro das chaves vai executar primeiro da aplicação
+    #endregion
 }
