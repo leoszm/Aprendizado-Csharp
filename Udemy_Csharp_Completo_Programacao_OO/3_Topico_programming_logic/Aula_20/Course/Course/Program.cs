faba795db380c7a5f2516e877e6444f61830908f
leoszm <leonardosousa60@gmail.com>
2023-04-09 00:34:20 -0300

    Udemy aula 20

---
+++ Udemy_Csharp_Completo_Programacao_OO/3_Topico_programming_logic/Aula_20/Course/Course/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conversão implícita de tipos
+
+            //float tem 4 bytes
+            float x = 4.5f;
+            //double tem 8, assim sendo, 4 cabem em 8
+            double y = x;
+            Console.WriteLine(y);
+            //Casting
+            double a =  5.1;
+            float b = (float)a;
+            //Erro, não é possível converter implicitamente, o compilador diz que vc pode mas pode perder dados no proceso,
+            //mas usando casting você diz que tem ciência que sabe que pode perder dados
+            //mas insiste que o compilador execute assim msm
+
+            //Outra situação entre operações
+            // Dois inteiros em uma divisão
+            int e = 5;
+            int f = 2;
+            //sendo o resultado em double(decimal)
+            double resultado = e / f;
+            Console.WriteLine(resultado);
+            //Motivo: o compilador entendeu que você quer um resultado em inteiro pois utilizou numeros inteiros
+            //Para resolver isso é necessário uso de casting
+            resultado = (double)e / f;
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
