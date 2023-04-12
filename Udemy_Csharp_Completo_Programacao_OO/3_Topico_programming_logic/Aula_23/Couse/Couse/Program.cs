af4cd4f5de2d3333f0f17aeaceb880ca20e35a82
leoszm <leonardosousa60@gmail.com>
2023-04-12 00:46:27 -0300

    Udemy aula 23

---
+++ Udemy_Csharp_Completo_Programacao_OO/3_Topico_programming_logic/Aula_23/Couse/Couse/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Parte 1
+            /*
+            //o readline é string, para converter para int é necesário utilizar o Parse
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Você digitou");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            */
+            #endregion
+            #region Parte 2
+            /*
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2",CultureInfo.InvariantCulture));
+            */
+            #endregion
+            #region Exercicio Proposto
+            
+            //int quartos = int.Parse(vet[2]);
+            //double preco = double.Parse(vet[3],CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu nome completo");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu últmo nome, idade e altura (mesma linha)");
+            string[] vet = Console.ReadLine().Split(' ');
+
+            string codenome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(codenome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+            #endregion
+        }
+    }
+}
