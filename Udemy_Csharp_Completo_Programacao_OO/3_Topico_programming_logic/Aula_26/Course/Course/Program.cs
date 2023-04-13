4884458cb3701c24b7df43668c9a603de5fb80f4
leoszm <leonardosousa60@gmail.com>
2023-04-13 05:54:07 -0300

    Udemy aula 26 operadores lógicos and reapir aula 25

---
+++ Udemy_Csharp_Completo_Programacao_OO/3_Topico_programming_logic/Aula_26/Course/Course/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 2 > 3 || 4 != 5; // true
+            bool c2 = !(2 > 3) && 4 != 5; // true
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine("--------------");
+            bool c3 = 10 < 5; // false
+            bool c4 = c1 || c2 && c3; // true
+            Console.WriteLine(c3);
+            Console.WriteLine(c4);
+        }
+    }
+}
