4b09a8d44351f099758f5622ba56a8cf5c8f59f1
leoszm <leonardosousa60@gmail.com>
2023-04-13 22:52:44 -0300

    Udemy aula 38 estrutura repetitiva for

---
+++ Udemy_Csharp_Completo_Programacao_OO/4_Topico_class_attributes_methods_static_members/Aula_38/Course/Course/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            xA = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pX = (xA + xB + xC) / 2;
+            double pY = (yA + yB + yC) / 2;
+
+            double areaX = Math.Sqrt(pX * (pX - xA)*(pX - xB)*(pX - xC)); 
+            double areaY = Math.Sqrt(pY * (pY - yA)*(pY - yB)*(pY - yC));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
