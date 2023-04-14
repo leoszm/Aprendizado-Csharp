1cf6b8078f2eecb4ffba1b63e0ebad2adff7faae
leoszm <leonardosousa60@gmail.com>
2023-04-13 21:10:44 -0300

    Udemy aula 28 escopo e inicialização

---
+++ Udemy_Csharp_Completo_Programacao_OO/3_Topico_programming_logic/Aula_28/Course/Course/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Errado variável x estar sendo chamada no WriteLine
+             * sem ter sido atribuído nenhum valor.
+          
+            int x;
+            Console.WriteLine(x);
+            */
+
+            /* Errado devido a variável desconto estar sendo declarada dentro
+             * da estrutura, fora da estrutura ela não existe...
+             
+             * De maneira técnica, o escopo maior(função, projeto, etc)
+             * não reconhece escopo menor, ja o escopo menor 
+             * reconhece o maior...
+             
+            double preco = double.Parse(Console.ReadLine());
+            if (preco > 100.0)
+            {
+                double desconto = preco * 0.1;
+            }
+            Console.WriteLine(desconto);
+            */
+        }
+    }
+}
