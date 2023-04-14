d9f7b2a2ab143f83089b02c6eea4a5683d4c314b
leoszm <leonardosousa60@gmail.com>
2023-04-13 22:30:41 -0300

    Udemy aula 34 estrutura repetitiva for

---
+++ Udemy_Csharp_Completo_Programacao_OO/3_Topico_programming_logic/Aula_34/Course/Course/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos números inteiros você vai digitar?");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for(int i = 1;i <= n; i++)
+            {
+                Console.WriteLine("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine("O resultado é = " + soma);
+        }
+    }
+}
