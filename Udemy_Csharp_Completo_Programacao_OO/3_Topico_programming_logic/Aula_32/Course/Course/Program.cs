20e96764e488b10770993b128f1431cf279330dd
leoszm <leonardosousa60@gmail.com>
2023-04-13 22:14:55 -0300

    Udemy aula 32 estrutura repetitiva while

---
+++ Udemy_Csharp_Completo_Programacao_OO/3_Topico_programming_logic/Aula_32/Course/Course/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (x >= 0.0){
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Digite outro número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Número negativo!");
+        }
+    }
+}
