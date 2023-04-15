614ffeb2aa4248abc0ecff2bbba5655fbff83acb
leoszm <leonardosousa60@gmail.com>
2023-04-14 23:27:22 -0300

    Udemy aula 39 Criando uma classe com tres atributos...

---
+++ Udemy_Csharp_Completo_Programacao_OO/4_Topico_class_attributes_methods_static_members/Aula_39/Course/Course/Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
