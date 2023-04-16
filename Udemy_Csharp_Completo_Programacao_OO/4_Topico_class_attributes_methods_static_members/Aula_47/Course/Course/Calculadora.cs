46baa71b091db4a590f35e77edc7a92667cd4dfa
leoszm <leonardosousa60@gmail.com>
2023-04-16 17:23:36 -0300

    Udemy aula 47 membros estaticos parte 2

---
+++ Udemy_Csharp_Completo_Programacao_OO/4_Topico_class_attributes_methods_static_members/Aula_47/Course/Course/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public static double Volume(double r)
+        {
+            return (4.0 / 3) * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
