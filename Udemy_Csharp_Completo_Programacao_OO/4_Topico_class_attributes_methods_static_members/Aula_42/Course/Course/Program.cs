6fc34884fa8430c381df26a0539f7b7be936b17f
leoszm <leonardosousa60@gmail.com>
2023-04-16 16:17:24 -0300

    Udemy aula 42 comecando a resolver um..., 43 Object e ToString

---
+++ Udemy_Csharp_Completo_Programacao_OO/4_Topico_class_attributes_methods_static_members/Aula_42/Course/Course/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+        }
+        #region Comentários
+        /*
+        * Toda classe em C# é uma subclasse da classe Object;
+
+        * Object possui os seguintes métodos:
+            -GetType - retorna o tipo do objeto;
+            -Equals - Compara se o objeto é igual a outro;
+            -GetHashCode - retorna um código hash do objeto;
+            -ToString - converte o objeto para string;
+         */
+        #endregion
+    }
+}
