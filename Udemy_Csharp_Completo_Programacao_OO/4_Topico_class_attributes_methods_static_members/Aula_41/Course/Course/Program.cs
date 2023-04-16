f882a3a60ff39e285093a0d12185a876128c709e
leoszm <leonardosousa60@gmail.com>
2023-04-16 15:13:13 -0300

    Udemy aula 41 criando um metodo para obtermos os beneficios....

---
+++ Udemy_Csharp_Completo_Programacao_OO/4_Topico_class_attributes_methods_static_members/Aula_41/Course/Course/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //areaX = triângulo(classe) x(com valores a,b,c) fazendo a operação de Area
+            double areaX = x.Area();
+            //areaY = triângulo(classe) Y(com valores a,b,c) fazendo a operação de Area
+            double areaY = y.Area();
+
+            Console.WriteLine("A area do triangulo X é " + areaX.ToString("F4"), CultureInfo.InvariantCulture);
+            Console.WriteLine("A area do triangulo Y é " + areaY.ToString("F4"), CultureInfo.InvariantCulture);
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área Y");
+            }
+        }
+    }
+}
---
+++ Udemy_Csharp_Completo_Programacao_OO/4_Topico_class_attributes_methods_static_members/Aula_41/Course/Course/Triangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
