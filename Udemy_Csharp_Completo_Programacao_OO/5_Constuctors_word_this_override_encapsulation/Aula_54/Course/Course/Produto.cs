794f8d7deb283b1e16f9894dffec06cad453f0b1
leoszm <leonardosousa60@gmail.com>
2023-04-16 23:06:15 -0300

    Udemy aula 54 Palavra this

---
+++ Udemy_Csharp_Completo_Programacao_OO/5_Constuctors_word_this_override_encapsulation/Aula_54/Course/Course/Produto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+            Quantidade = 10;
+        }
+        //recebe o conteudo de cima para não repetir o codigo
+        public Produto(string nome, double preco) : this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+        //recebe o conteudo de cima especificado dentro do this para não repetir o codigo
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        //método publico
+        //override indica que essa operação veio de outra classe
+        //string que é o tipo de saída da função
+        //ToString é o nome da função
+        public override string ToString()
+        {
+            return Nome
+                + ", R$"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade
+                + " unidades, Total: R$"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        //void significa vazio, indicando que não vai retornar nada como saída
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;//Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;//Quantidade = Quantidade - quantidade;
+        }
+    }
+}
---
+++ Udemy_Csharp_Completo_Programacao_OO/5_Constuctors_word_this_override_encapsulation/Aula_54/Course/Course/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto();
+
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+        #region Comentários
+        /*
+         * This
+            - É uma referência para o Próprio objeto
+            - Usos Comuns:
+                1. Diferenciar atributos de variáveis locais(Java)
+                2. Referenciar outro construtor em um construtor
+                3. Passar o próprio objeto como argumento na chamada de um método ou construtor
+         */
+        #endregion
+    }
+}
