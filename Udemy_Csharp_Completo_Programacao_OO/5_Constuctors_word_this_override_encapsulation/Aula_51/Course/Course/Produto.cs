06d33937f5598be4c8ca4a5a95dd0ef80b6ba26a
leoszm <leonardosousa60@gmail.com>
2023-04-16 22:06:14 -0300

    Udemy new folder 5 topic and aula 51 construtores

---
+++ Udemy_Csharp_Completo_Programacao_OO/5_Constuctors_word_this_override_encapsulation/Aula_51/Course/Course/Produto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Course
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Construtor tem o mesmo nome da classe e a função dele foi não permitir que os parametros
+        //da classe Produto(Nome, Preco e Quantidade) continuassem nulos, ele recebe os valores da
+        //outra classe e atribui esses valores aos parametros dessa classe
+        public Produto (string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        //método publico
+        //override indica que essa operação veio de outra classe
+        //string que é o tipo de saída da função
+        //ToString é o nome da função
+        public override string ToString()
+        {
+            return Nome
+                + ", R$"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade
+                + " unidades, Total: R$"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        //void significa vazio, indicando que não vai retornar nada como saída
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;//Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;//Quantidade = Quantidade - quantidade;
+        }
+    }
+}
---
+++ Udemy_Csharp_Completo_Programacao_OO/5_Constuctors_word_this_override_encapsulation/Aula_51/Course/Course/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+            Console.WriteLine("-------------------------------------------------------------");
+            
+        }
+        #region Comentários
+        /*
+         *Construtor:
+            - é uma operação especial de classe, que executa no momento 
+                da instanciação do objeto;
+            - Usos Comuns:
+                1. Iniciar valores dos atributos;
+                2. Permitir ou obrigar que o objeto receba dados/dependências
+                    no momento de sua instanciação(injeção de dependência);
+            - Se um construtor customizado não for especificado, a classe
+                disponibiliza o construtor padrão:
+                1. exemplo: Produto p = new Produto();
+            - Também é possível especificar mais de um construtor na mesma 
+                classe(sobrecarga);
+         
+         
+         
+         */
+
+        #endregion
+    }
+}
