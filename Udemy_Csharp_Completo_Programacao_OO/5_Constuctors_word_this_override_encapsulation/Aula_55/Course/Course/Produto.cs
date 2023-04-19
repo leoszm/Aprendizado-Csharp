adfb5cb59cf348acb96b2bec83c5dfbfb6bec303
leoszm <leonardosousa60@gmail.com>
2023-04-19 00:04:36 -0300

    Udemy aula 55 Encapsulamento

---
+++ Udemy_Csharp_Completo_Programacao_OO/5_Constuctors_word_this_override_encapsulation/Aula_55/Course/Course/Produto.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto(){
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if(nome!=null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        //método publico
+        //override indica que essa operação veio de outra classe
+        //string que é o tipo de saída da função
+        //ToString é o nome da função
+        public override string ToString()
+        {
+            return _nome
+                + ", R$"
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + _quantidade
+                + " unidades, Total: R$"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        //void significa vazio, indicando que não vai retornar nada como saída
+        public void AdicionarProduto(int quantidade)
+        {
+            _quantidade += quantidade;//Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            _quantidade -= quantidade;//Quantidade = Quantidade - quantidade;
+        }
+    }
+}
---
+++ Udemy_Csharp_Completo_Programacao_OO/5_Constuctors_word_this_override_encapsulation/Aula_55/Course/Course/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine(p.GetNome());//TV
+            Console.WriteLine("Escreva um novo nome");
+            p.SetNome("TV 4K");//Alterando
+            Console.WriteLine(p.GetNome());//TV 4K
+        }
+        #region Comentários
+        /*
+         * Encapsulamento:
+            - É um princípio que consiste em esconder detalhes de implementação de um
+              componente, expondo apenas operações seguras e que o mantenha em um
+              estado consistente.
+            - Regra de ouro: o objeto deve sempre estar em um estado consistente, e a
+              própria classe deve garantir isso.
+         */
+        #endregion
+    }
+}
