cd5f0f55246666d6c837a5142bae63e9a0ffff09
leoszm <leonardosousa60@gmail.com>
2023-04-30 12:57:02 -0300

    Udemy aula 57 Auto properties

---
+++ Udemy_Csharp_Completo_Programacao_OO/5_Constuctors_word_this_override_encapsulation/Aula_57/Course/Course/Produto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Produto
+    {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto ()
+        {
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        #region Substituido public double Preco
+        //public double Preco
+        //{
+        //    get { return _preco; }
+        //}
+
+        //public int Quantidade
+        //{
+        //    get { return _quantidade; }
+        //}
+        #endregion
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", R$"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade
+                + " unidades, Total: R$"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+    }
+}
---
+++ Udemy_Csharp_Completo_Programacao_OO/5_Constuctors_word_this_override_encapsulation/Aula_57/Course/Course/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+            Console.WriteLine(p.Nome);
+            /*
+             * A mesma propriedade da o Get do nome abaixo e caso esteja dentro do
+             * Permitido, dá o set
+            */
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+        #region Comentários
+        /*
+         * Auto Properties(propriedades autoimplementadas):
+            - É uma forma simplificada de se declarar propriedades que não necessitam
+              lógicas particulares para as operações get e set.
+            - Quando o objeto exigir algum tipo de lógica particular, deverá ser utilizada
+              a Propertie comum, explicada na aula 56 do curso da Udemy
+         */
+        #endregion
+    }
+}
