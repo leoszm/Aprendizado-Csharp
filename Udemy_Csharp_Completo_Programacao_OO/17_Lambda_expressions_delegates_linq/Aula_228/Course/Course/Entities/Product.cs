26423958da6e88f092584a5f166054d6458135a8
Leonardo Souza Martins <LSMartins@santoandre.sp.gov.br>
2023-05-05 15:12:10 -0300

    aula 228 comparison

---
+++ Udemy_Csharp_Completo_Programacao_OO/17_Lambda_expressions_delegates_linq/Aula_228/Course/Course/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+
+    //Classe product implementa classe Genérica de IComparable
+    class Product /*: IComparable<Product>*/
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        //Comparando o nome de um produto com o outro pelo nome desconsiderando maiúsculas e minúsculas
+        //public int CompareTo(Product other)
+        //{
+        //    //Convertendo para letra maiúscula
+        //    return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        //}
+    }
+}
---
+++ Udemy_Csharp_Completo_Programacao_OO/17_Lambda_expressions_delegates_linq/Aula_228/Course/Course/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
+
+namespace Course {
+
+    #region parte 1 - Ordenar a lista
+    //class Program {
+    //    static void Main(string[] args) {
+
+    //        //Lista de produtos
+    //        List<Product> list = new List<Product>();
+
+    //        //Adicionando 3 produtos
+    //        list.Add(new Product("TV", 900.00));
+    //        list.Add(new Product("Notebook", 1200.00));
+    //        list.Add(new Product("Tablet", 450.00));
+
+    //        //Ordenar a lista
+    //        list.Sort();
+
+    //        //Imprimir os produtos
+    //        foreach (Product p in list) {
+    //            Console.WriteLine(p);
+    //        }
+    //    }
+    //}
+    #region Comentários
+
+    /*
+     * Comparison<T>
+     */
+    #endregion
+    #endregion
+
+    #region Parte 2 - Dinamizar o programa, para que não tenha de alterar a classe para fazer comparação, e sim o elemento
+
+    /*class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Lista de produtos
+            List<Product> list = new List<Product>();
+
+            //Adicionando 3 produtos
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            //Uma variável que guarda a referência para uma função
+            Comparison<Product> comp = CompareProducts;
+
+            //Ordenar a lista usando a referência de um método, sem passar os parametros como argumentos entre parenteses, exemplo de um Delegate
+            //list.Sort(CompareProducts);
+
+            //Recebe essa referência como argumento da função sort
+            list.Sort(comp);
+
+            //Imprimir os produtos
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        //Método auxiliar que compara 2 produtos
+        static int CompareProducts(Product p1, Product p2){
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        }
+    }*/
+    #region Comentários
+    //Delegate é uma referência para função com type Safety
+    /*
+     * Comparison<T> é um Delegate
+        - Sua declaração é realizada desta forma:
+            . public delegate int Comparison<in T>(T x, T y);
+            . public delegate int Comparison do tipo T recebendo 2 objetos do tipo T
+            . Comparison é genérico e retorna um tipo inteiro
+     * 
+     */
+    #endregion
+    #endregion
+
+    #region Parte 3 - Passar o CompareProducts como função lambda
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Lista de produtos
+            List<Product> list = new List<Product>();
+
+            //Adicionando 3 produtos
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            //Não precisa declarar objetos como tipo Product, eles são inferidos, e após eles colocar a seta de função que será a expressão resultante desses dois argumentos
+            //Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            //Recebe essa referência como argumento da função sort
+            //list.Sort(comp);
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            //Imprimir os produtos
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+    }
+
+    #endregion
+}
